6536428a7e1c3938e419ad68adb6491a29a573ef
Jordan Thomas <me@jordanthomas.org>
2010-11-22 20:07:40 -0500

    Adds config

---
+++ config/authorizenet.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+	'default' => array
+	(
+		'login' => '',
+		'tran_key' => '',
+		'test_mode' => TRUE,
+		'duplicate_window' => 60
+	));
